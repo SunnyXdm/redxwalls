bcc51923409c507e886eb17c27942d0d59ff37dd
Sunny /dx/ <45434553+SunnyXdm@users.noreply.github.com>
2021-10-04 10:54:23 +0000

    > Created initial Vitejs React project. > Added Tailwindcss with JIT. > Added default homepage.

---
+++ src/Home.jsx
@@ -0,0 +1,134 @@
+function Home() {
+  return (
+    <div className="bg-gray-900">
+      <div className="p-5 font-poppins text-indigo-600 text-4xl">ReDxWalls</div>
+      <div className="flex justify-center">
+        <input
+          className="
+            rounded-full
+            bg-white
+            bg-opacity-0
+            w-10/12 h-12
+            outline-none
+            ring-gray-400
+            ring-2
+            focus:ring-indigo-600
+            focus:ring-2
+          "
+        ></input>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex-initial min-w-screen">
+          <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8 p-4">
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/wallpapers
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/wallpaper
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/multiwall
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/designporn
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/ImaginaryLandscapes
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/ImaginarySeascapes
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/ImaginaryColorscapes
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/aestheticrain
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/designporn
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/earthporn
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/spaceporn
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/amoled
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              >
+                r/designporn
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="min-h-screen flex items-center"></div>
+    </div>
+  );
+}
+export default Home;
