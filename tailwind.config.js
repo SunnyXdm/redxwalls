fb1332bec44f2f082e84e44b8ba38d776fd26189
Sunny /dx/ <45434553+SunnyXdm@users.noreply.github.com>
2021-10-06 12:07:40 +0000

    Added: Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+            },
+        },
+    },
   variants: {
     extend: {},
   },
